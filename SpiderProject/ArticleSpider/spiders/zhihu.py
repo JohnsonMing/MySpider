3b4dc17cb2b826f43d303e9403232b69cfb5b695
JohnsonMing <17826892839@163.com>
2017-09-16 21:34:50 +0800

    项目备份

--- SpiderProject/ArticleSpider/spiders/zhihu.py
+++ SpiderProject/ArticleSpider/spiders/zhihu.py
@@ -5,7 +5,7 @@ import scrapy
 class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     allowed_domains = ['www.zhihu.com']
-    start_urls = ['http://www.zhihu.com/']
+    start_urls = ['https://www.zhihu.com/']
 
     def parse(self, response):
         pass
