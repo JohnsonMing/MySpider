5f2fa801392a9b948375c767424561733796ce34
JohnsonMing <17826892839@163.com>
2017-09-17 13:04:15 +0800

    scrapy模拟登录知乎
    利用scrapy模拟登录知乎

--- SpiderProject/ArticleSpider/spiders/zhihu.py
+++ SpiderProject/ArticleSpider/spiders/zhihu.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
+import re
 
 
 class ZhihuSpider(scrapy.Spider):
@@ -7,5 +9,45 @@ class ZhihuSpider(scrapy.Spider):
     allowed_domains = ['www.zhihu.com']
     start_urls = ['https://www.zhihu.com/']
 
+    headers = {
+        "HOST": "www.zhihu.com",
+        "Referer": "https://www.zhihu.com",
+        'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0"
+    }
+
     def parse(self, response):
         pass
+
+    def parse_detail(self, response):
+        pass
+
+    def start_requests(self):
+        return [scrapy.Request('https://www.zhihu.com/#signin', headers=self.headers, callback=self.login)]
+
+    def login(self, response):
+        response_text = response.text
+        match_obj = re.match('.*name="_xsrf" value="(.*?)"', response.text, re.DOTALL)
+        xsrf = ''
+        if match_obj:
+            xsrf = (match_obj.group(1))
+
+        post_url = "https://www.zhihu.com/login/phone_num"
+        post_data = {
+            "_xsrf": xsrf,
+            "phone_num": "***",
+            "password": "***"
+        }
+
+        return [scrapy.FormRequest(
+            url="https://www.zhihu.com/login/phone_num",
+            formdata=post_data,
+            headers=self.headers,
+            callback=self.check_login
+        )]
+
+    def check_login(self, response):
+        # 验证服务器的返回数据是否成功
+        text_json = json.loads(response.text)
+        if "msg" in text_json and text_json["msg"] == "登录成功":
+            for url in self.start_urls:
+                yield scrapy.Request(url, dont_filter=True, headers=self.headers)
