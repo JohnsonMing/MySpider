dc834d02eb9e1c8e92e08f78b11b52880c611a0e
JohnsonMing <17826892839@163.com>
2017-09-18 15:32:28 +0800

    添加处理新版本页面

--- SpiderProject/ArticleSpider/spiders/zhihu.py
+++ SpiderProject/ArticleSpider/spiders/zhihu.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.loader import ItemLoader
 import json
 import re
+
+try:
+    import urlparse as parse
+except:
+    from urllib import parse
+from items import ZhihuQuestionItem, ZhihuAnswerItem
 
 
 class ZhihuSpider(scrapy.Spider):
@@ -16,10 +23,45 @@ class ZhihuSpider(scrapy.Spider):
     }
 
     def parse(self, response):
-        pass
+        # 提取出html页面中的所有url, 并跟踪这些url进行进一步爬取
+        # 如果提取的url中的格式为/question/xxx 就下载之后直接进入解析函数
 
-    def parse_detail(self, response):
-        pass
+        all_urls = response.css("a::attr(href)").extract()
+        all_urls = [parse.urljoin(response.url, url) for url in all_urls]
+        all_urls = filter(lambda x: True if x.startwith("https") else False, all_urls)
+        for url in all_urls:
+            print(url)
+            match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", url)
+            if match_obj:
+                request_url = match_obj.group(1)
+                question_id = match_obj.group(2)
+
+                yield scrapy.Request(request_url, headers=self.headers, callback=self.parse_question)
+
+    def parse_question(self, response):
+        # 处理question页面，从页面中提取具体的quesiton item
+
+        if "QuestionHeader-title" in response.text:
+            # 处理新版本的页面
+            match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", response.url)
+            if match_obj:
+                question_id = int(match_obj.group(2))
+
+            item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
+            item_loader.add_css("title", "h1.QuestionHeader-title::text")
+            item_loader.add_css("content", ".QuestionHeader-detail")
+            item_loader.add_value("url", response.url)
+            item_loader.ad_value("zhihu_id", question_id)
+            item_loader.add_css("answer_num", ".List-headerText span::text")
+            item_loader.add_css("comments_num", "Questionheader-actions button::text")
+            item_loader.add_css("watch_user_num", ".NumberBoard-value::text")
+            item_loader.add_css("topics", ".QuestionHeader-topics .Popover::text")
+
+            question_item = item_loader.load_item()
+
+        else:
+            # 处理知乎旧版本
+            pass
 
     def start_requests(self):
         return [scrapy.Request('https://www.zhihu.com/#signin', headers=self.headers, callback=self.login)]
